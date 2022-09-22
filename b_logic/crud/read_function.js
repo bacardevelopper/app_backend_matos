929c9ff163d5497683cb4dd62f0431fd14cfa708
niwradCodera <63006659+bacardevelopper@users.noreply.github.com>
2022-09-22 20:44:54 +0100

    find item function (ok)

--- b_logic/crud/read_function.js
+++ b_logic/crud/read_function.js
@@ -1,23 +1,31 @@
 let model_matos = require("../../models/model_config_objet");
-let Model_user = require("../../models/model_config_user");
-exports.fct_read = (req, res, next) => {
-    return res.status(200).json({ message: "fonction read ok" });
-  };
-  
-  exports.fct_readAll = async (req, res, next) => {
-    let userDecoded = String(req.user_decoded.data);
-    let data = await Model_user.findOne({ _id: userDecoded });
-    let userNameRes = data.user_name;
-    let idUser = data._id;
-  
-    model_matos.find((err, doc) => {
-      if (doc) {
-        return res.status(201).json({ userNameRes, idUser, doc });
-      } else {
-        return res
-          .status(401)
-          .json({ message: "erreur verifier votre connexion" });
-      }
-    });
-  };
-  
+let model_user = require("../../models/model_config_user");
+const { print } = require("print_console_log");
+
+exports.fct_read = async (req, res, next) => {
+  let idItem = req.params.id;
+  let dataFindOne = await model_matos.findOne({ _id: idItem });
+
+  if (dataFindOne) {
+    return res.status(200).json(dataFindOne);
+  } else {
+    return res.status(401).json({ message: "dont find item" });
+  }
+};
+
+exports.fct_readAll = async (req, res, next) => {
+  let userDecoded = String(req.user_decoded.data);
+  let data = await model_user.findOne({ _id: userDecoded });
+  let userNameRes = data.user_name;
+  let idUser = data._id;
+
+  model_matos.find((err, doc) => {
+    if (doc) {
+      return res.status(201).json({ userNameRes, idUser, doc });
+    } else {
+      return res
+        .status(401)
+        .json({ message: "erreur verifier votre connexion" });
+    }
+  });
+};
