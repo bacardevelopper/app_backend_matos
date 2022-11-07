7e742bc1ddbbe6b94ca622511c572aa349a30870
niwradCodera <63006659+bacardevelopper@users.noreply.github.com>
2022-11-07 09:33:11 +0100

    fonctionnalité de modification apporté

--- b_logic/crud/create_function.js
+++ b_logic/crud/create_function.js
@@ -2,23 +2,24 @@ const { print } = require("print_console_log");
 let model_matos = require("../../models/model_config_objet");
 
 exports.fct_create = (req, res, next) => {
-    print(req.body);
-  
-    // nouveaux modele
-    let model_post = new model_matos({
-      nom: req.body.nom,
-      avis: req.body.avis,
+  print(req.body);
+
+  // nouveaux modele
+  let model_post = new model_matos({
+    nom: req.body.nom,
+    avis: req.body.avis,
+    lieu: "",
+  });
+
+  const save_data = () => {
+    model_post.save((err, doc) => {
+      if (doc) {
+        return res.status(201).json({ message: "fonction create ok" });
+      } else {
+        return res.status(401).json({ message: "non enregistré" });
+      }
     });
-  
-    const save_data = () => {
-      model_post.save((err, doc) => {
-        if (doc) {
-          return res.status(201).json({ message: "fonction create ok" });
-        } else {
-          return res.status(401).json({ message: "non enregistré" });
-        }
-      });
-    };
-  
-    save_data();
   };
+
+  save_data();
+};
--- b_logic/crud/update_function.js
+++ b_logic/crud/update_function.js
@@ -2,18 +2,31 @@ let model_matos = require("../../models/model_config_objet");
 const { print } = require("print_console_log");
 const { findUserIdAndName } = require("../../functions_bdd_users/bdd_user");
 
+let msgReussite = "modification apporté";
+let msgEchec = "modification non apporté";
 exports.fct_updateItem = async (req, res, next) => {
+  print(req.body);
   let idUser = req.user_decoded.data;
   let idItem = req.body.id_item;
   let dateReq = req.body.date;
-  let pseudoName =  await findUserIdAndName(idUser);
+  let avisReq = req.body.avis;
+  let lieuReq = req.body.lieu;
+  let pseudoName = await findUserIdAndName(idUser);
   pseudoName = pseudoName.user_name;
   let dataPush = { pseudo: pseudoName, dateReq };
-  
+
   let matos = await model_matos.findOne({ _id: idItem });
+  await matos.updateOne({ avis: avisReq, lieu: lieuReq }, (err, doc) => {
+    if (err) {
+      return res.status(400).json({ message: msgEchec });
+    } else {
+      return res.status(200).json({ message: msgReussite });
+    }
+  });
   matos.flux.push(dataPush);
   await matos.save();
 
-  return res.status(200).json({message : "modification apporté"});
+  return res.status(200).json({ message: msgReussite });
 };
+
 exports.fct_updateMany = (req, res, next) => {};
